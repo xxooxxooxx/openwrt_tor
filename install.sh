c461ae8a85cd1a196676a119e9283602c35f79fe
xxooxxooxx <panchunmei30@gmail.com>
2020-01-02 20:34:01 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,28 +13,30 @@ opkg update
 opkg install tor ca-bundle ca-certificates bash python kmod-tcp-bbr libustream-openssl
 mkdir /opt>/dev/null 2>&1
 cd /opt
-wget https://raw.githubusercontent.com/torproject/tor/master/src/config/mmdb-convert.py
-wget https://raw.githubusercontent.com/xr09/cron-last-sunday/master/run-if-today
-chmod +x run-if-today
+wget https://raw.githubusercontent.com/torproject/tor/master/src/config/geoip
+wget https://raw.githubusercontent.com/torproject/tor/master/src/config/geoip6
+#wget https://raw.githubusercontent.com/torproject/tor/master/src/config/mmdb-convert.py
+#wget https://raw.githubusercontent.com/xr09/cron-last-sunday/master/run-if-today
+#chmod +x run-if-today
 
-cat >updateip.sh<<EOF
-#!/bin/bash
+#cat >updateip.sh<<EOF
+##!/bin/bash
 
-source /etc/profile
-cd /opt
-wget https://geolite.maxmind.com/download/geoip/database/GeoLite2-Country.mmdb.gz && \
-gunzip GeoLite2-Country.mmdb.gz && \
-python mmdb-convert.py GeoLite2-Country.mmdb && \
-/etc/init.d/tor restart
-rm GeoLite2-Country.mmdb>/dev/null 2>&1
-EOF
-chmod +x updateip.sh
+#source /etc/profile
+#cd /opt
+#wget https://geolite.maxmind.com/download/geoip/database/GeoLite2-Country.mmdb.gz && \
+#gunzip GeoLite2-Country.mmdb.gz && \
+#python mmdb-convert.py GeoLite2-Country.mmdb && \
+#/etc/init.d/tor restart
+#rm GeoLite2-Country.mmdb>/dev/null 2>&1
+#EOF
+#chmod +x updateip.sh
 
-cat >cron<<EOF
-5 6 * * 4 /opt/run-if-today 3 && /opt/updateip.sh
-EOF
-crontab cron
-rm cron>/dev/null 2>&1
+#cat >cron<<EOF
+#5 6 * * 4 /opt/run-if-today 3 && /opt/updateip.sh
+#EOF
+#crontab cron
+#rm cron>/dev/null 2>&1
 
 cat<<EOF>>/etc/tor/torrc
 SocksPort `ip a |grep br-lan | grep '\binet\b.*\/[[:digit:]]\{1,2\}.*\bscope[[:space:]]global\b.*' | awk '{print $2}' | cut -d "/" -f 1`:9050
@@ -49,4 +51,5 @@ ExcludeExitNodes {cn},{hk},{mo},{sg},{th},{pk},{by},{ru},{ir},{vn},{ph},{my},{cu
 #EntryNodes {us}
 #ExitNodes {us}
 EOF
-./updateip.sh
+#./updateip.sh
+/etc/init.d/tor restart
